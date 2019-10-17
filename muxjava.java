4373348e128b32de664f748ee5dd7c358057df8c
karankiritkumar <47301964+karankiritkumar@users.noreply.github.com>
2019-10-17 17:23:41 -0500

    Update muxjava.java

--- muxjava.java
+++ muxjava.java
@@ -1,4 +1,26 @@
 
-public class muxjava {
+public class muxjava
+{
+	
+	
+	private void DisplayTabble(String[] input) 
+	{
+		
+		
+	}
+	
+	
+	
+	
+public public static void main(String[] args) 
+{
+	muxjava j = new muxjava();
+	String[] input = new String[] { "a0","a1","a2","a3","a4"};
+	j.DisplayTabble(input);
+	
+	
+	
+}
+
 
 }
