1093c56cc6c827038ccd053fcbd23ae45a071d29
karankiritkumar <47301964+karankiritkumar@users.noreply.github.com>
2019-10-17 21:08:48 -0500

    new

--- CompositeMemoryVertical.java
+++ CompositeMemoryVertical.java
@@ -1,4 +1,14 @@
 
-public class CompositeMemoryVertical {
-
+public class CompositeMemoryVertical
+{
+	public class CompositeMemoryVertical {
+		private Memory[] chips;
+		private int size;
+		private int bitwidth;
+		
+		public CompositeMemoryVertical(int chip, int size, int bitwidth) {
+			chips = new Memory[chip];
+			for(int i = 0; i < chip; i++) {
+				chips[i] = new Memory(size, bitwidth);
+			}
 }
--- Memory.java
+++ Memory.java
@@ -1,4 +1,16 @@
 
 public class Memory {
-
+	public class Memory {
+		private LinkedList<MemoryBucket> buckets;
+		private int bitwidth = 0;
+		
+		public Memory(int bitwidth, int size) 
+		{
+			this.bitwidth = bitwidth;
+			buckets = new LinkedList<MemoryBucket>();
+			for(int i = 0; i < size; i++) {
+				MemoryBucket mb = new MemoryBucket(i, bitwidth);
+				buckets.add(mb);
+			}
+		}
 }
